9322fe74749ef0d04dcf71771fdbfb5502ffc556
Mauro Delgado <maurods@uamv.edu.ni>
2025-08-29 09:08:51 -0600

    Add files via upload

---
+++ Agosto/Numeros/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num > 0)
+{
+    Console.WriteLine("El número ingresado es positivo");
+}
+else if (num < 0)
+{
+    Console.WriteLine("El número ingresado es negativo");
+}
+else if (num == 0)
+{
+    Console.WriteLine("El nuúmero ingresado es 0");
+}
